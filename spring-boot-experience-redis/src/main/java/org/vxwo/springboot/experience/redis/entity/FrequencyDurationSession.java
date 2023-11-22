183bd7c998926f01047326079a95520eb1488b34
Vietor Liu <vietor.liu@gmail.com>
2023-11-22 20:52:52 +0800

    Clean verify warning

--- spring-boot-experience-redis/src/main/java/org/vxwo/springboot/experience/redis/entity/FrequencyDurationSession.java
+++ spring-boot-experience-redis/src/main/java/org/vxwo/springboot/experience/redis/entity/FrequencyDurationSession.java
@@ -3,6 +3,10 @@ package org.vxwo.springboot.experience.redis.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * @author vxwo-team
+ */
+
 @Data
 @AllArgsConstructor
 public class FrequencyDurationSession {
--- spring-boot-experience-web/src/main/java/org/vxwo/springboot/experience/web/util/SplitUtil.java
+++ spring-boot-experience-web/src/main/java/org/vxwo/springboot/experience/web/util/SplitUtil.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * @author vxwo-team
+ */
+
 public final class SplitUtil {
 
     /**
